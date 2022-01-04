055e315e37cbe4d43fdf23fa5135b0f80e39797c
Jonny Heggheim <hegjon@gmail.com>
2022-01-04 22:40:36 +0100

    Fixed broken unit test

--- test/lbry_seeder/rpc_test.clj
+++ test/lbry_seeder/rpc_test.clj
@@ -6,4 +6,4 @@
 (deftest rpc
   (testing "claim_search"
     (is (= (request "@MoneroMatteo:b")
-          "{\"method\":\"claim_search\",\"params\":{\"channel\":\"@MoneroMatteo:b\"}}"))))
+          "{\"method\":\"claim_search\",\"params\":{\"channel\":\"@MoneroMatteo:b\",\"stream_type\":\"video\"}}"))))
